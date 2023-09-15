1737bea57977089c37c70f4c9d9949ab3ee3d5a7
shinyiiii <89680744+shinyiiii@users.noreply.github.com>
2023-09-15 14:49:54 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,37 +9,7 @@ def main():
 
     # Button to generate recommendations
     if st.button("Generate Recommendations"):
-        # Generate and display recommendations
+      
         recommendations = generate_recommendations(song_name)
         st.write(recommendations)
-        
-# Function to generate recommendations
-def generate_recommendations(song_name):
    
- # Filter songs by favorite artists
-    filtered_songs = df[df['artists'].apply(lambda x: any(artist in x for artist in user_input["favorite_artists"]))]
-    
-    
-    # Sort the filtered songs by some criteria (e.g., popularity, release date)
-    sorted_songs = filtered_songs.sort_values(by="popularity", ascending=False)
-
-    # Get the top N recommended songs
-    recommended_songs = sorted_songs.head(user_input["number_of_recommendations"])
-
-    # You can return the recommended songs as a list of dictionaries
-    # Each dictionary should contain song information (e.g., name, artists)
-    recommendations = []
-    for index, row in recommended_songs.iterrows():
-        song_info = {
-            "name": row["name"],
-            "artists": row["artists"]
-        }
-        recommendations.append(song_info)
-
-    return recommendations
-    
-    recommendations = pd.DataFrame({"Song": ["Recommendation 1", "Recommendation 2", "Recommendation 3"]})
-    return recommendations
-
-if __name__ == "__main__":
-    main()        
