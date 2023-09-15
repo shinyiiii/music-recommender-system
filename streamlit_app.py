cf5efa0343ee51ada62ea72c492f90857e4cfcc4
shinyiiii <89680744+shinyiiii@users.noreply.github.com>
2023-09-15 15:14:44 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,15 @@ def main():
         st.write(recommendations)
         
 # Function to generate recommendations
-def generate_recommendations(song_name):
+def getRecommendations(userName, database):
+    #provides a list of user recommendations (if they exist) to user"""
+    bestUser = findBestUser(userName, database)
+    if bestUser==None or database[userName]==[]:
+        print("No recommendations available at this time")
+    else:
+        recommendations = exclusiveListTwo(database[userName], database[bestUser])
+        for thing in recommendations:
+            print(thing)
 
 # Filter songs by favorite artists
     filtered_songs = df[df['artists'].apply(lambda x: any(artist in x for artist in user_input["favorite_artists"]))]
